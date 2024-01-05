4b753bd263a5d6cf4dd3433c1768c07f33cfeb94
eun0leee <eun0leee@gmail.com>
2024-01-05 20:22:54 +0900

    chore : font, color 설정 (#1)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,18 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    fontFamily: {},
     extend: {
-      colors: {},
+      colors: {
+        red: '#DF3223', // text-delete
+        blue: '#5C90F6', // section-text, button-background
+        white: '#FFFFFF', // text-default
+        'gray-300': '#DEDEDE', // text-title
+        'gray-400': '#B0B0B0', // icon-light, text-memo-preview
+        'gray-600': '#747474', // icon-dark
+        'gray-900': '#2D2E31', // list-active
+        'gray-930': '#1E1F20', // background
+        'gray-950': '#0F0F14', // line-divider
+      },
     },
   },
   plugins: [
