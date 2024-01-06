033f2c5c8a4fd88a34970ed4523e2f0bea862220
eun0leee <eun0leee@gmail.com>
2024-01-07 00:12:59 +0900

    fix : 레이아웃 nav 마크업 수정 (#4)

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Link, Outlet, useLocation } from 'react-router-dom';
 
 import iconAddBlue from '@/assets/icon_add_blue.svg';
 import iconArrowRight from '@/assets/icon_arrow_right.svg';
@@ -37,24 +37,32 @@ const Layout = () => {
         {navToggle ? (
           <nav className="w-56 border-r border-r-gray-950">
             <ol>
-              <li
-                className={`mb-2 flex cursor-pointer items-center px-3 py-2  hover:bg-gray-900 ${
-                  isAllNotePage ? 'bg-gray-900' : undefined
-                }`}
-              >
-                <button type="button" className="mr-2 h-5 w-4">
-                  <img src={iconNotes} alt="icon notes" />
-                </button>
-                <span className="font-semibold text-gray-300">All Notes</span>
+              <li className="mb-2">
+                <Link
+                  type="button"
+                  className={`flex w-full cursor-pointer items-center px-3  py-2 hover:bg-gray-900 ${
+                    isAllNotePage ? 'bg-gray-900' : undefined
+                  }`}
+                  to="/allnotes"
+                >
+                  <span className="mr-2 flex h-5 w-4 items-center">
+                    <img src={iconNotes} alt="icon notes" />
+                  </span>
+                  <span className="font-semibold text-gray-300">All Notes</span>
+                </Link>
               </li>
               <li className="flex items-center justify-between px-3 py-2">
                 <div className="flex items-center">
                   <button type="button" className="svg-container mr-2 h-5 w-4">
                     <img src={iconArrowRight} alt="icon arrow right" />
                   </button>
-                  <span className="cursor-pointer font-semibold text-blue-500 hover:text-blue-400">
+                  <Link
+                    type="button"
+                    className="cursor-pointer font-semibold text-blue-500 hover:text-blue-400"
+                    to="/notebooks"
+                  >
                     NOTEBOOKS
-                  </span>
+                  </Link>
                 </div>
                 <button type="button" className="mr-2 h-5 w-5">
                   <img src={iconAddBlue} alt="icon add blue" />
