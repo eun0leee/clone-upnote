f16b1afbd27661c8f85746d63815db954d13d716
eun0leee <eun0leee@gmail.com>
2024-01-06 15:56:08 +0900

    feat : nav 토글 버튼 추가 (#4)

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import iconAddBlue from '@/assets/icon_add_blue.svg';
@@ -6,10 +7,20 @@ import iconHamburger from '@/assets/icon_hamburger.svg';
 import iconNotes from '@/assets/icon_notes.svg';
 
 const Layout = () => {
+  const [navToggle, setNavToggle] = useState(true);
+
+  const handleNavToggleBtn = () => {
+    setNavToggle((prev) => !prev);
+  };
+
   return (
     <div className="h-screen">
       <header className="flex h-11 items-center justify-between border-b border-b-gray-950 pl-3 pr-8">
-        <button type="button" className="svg-container h-5 w-4 ">
+        <button
+          type="button"
+          className="svg-container h-5 w-4"
+          onClick={handleNavToggleBtn}
+        >
           <img src={iconHamburger} alt="icon hamburger" />
         </button>
         <button
@@ -20,31 +31,33 @@ const Layout = () => {
         </button>
       </header>
       <div className="flex h-[calc(100vh-44px)]">
-        <nav className="w-52 border-r border-r-gray-950">
-          <ol>
-            <li className="mb-2 flex items-center px-3 py-2 hover:bg-gray-900">
-              <button type="button" className="mr-2 h-5 w-4">
-                <img src={iconNotes} alt="icon notes" />
-              </button>
-              <span className="cursor-pointer font-semibold text-gray-300">
-                All Notes
-              </span>
-            </li>
-            <li className="flex items-center justify-between px-3 py-2">
-              <div className="flex items-center">
-                <button type="button" className="svg-container mr-2 h-5 w-4">
-                  <img src={iconArrowRight} alt="icon arrow right" />
+        {navToggle ? (
+          <nav className="w-52 border-r border-r-gray-950">
+            <ol>
+              <li className="mb-2 flex items-center px-3 py-2 hover:bg-gray-900">
+                <button type="button" className="mr-2 h-5 w-4">
+                  <img src={iconNotes} alt="icon notes" />
                 </button>
-                <span className="cursor-pointer font-semibold text-blue hover:text-[#5B7FCD]">
-                  NOTEBOOKS
+                <span className="cursor-pointer font-semibold text-gray-300">
+                  All Notes
                 </span>
-              </div>
-              <button type="button" className="mr-2 h-5 w-5">
-                <img src={iconAddBlue} alt="icon add blue" />
-              </button>
-            </li>
-          </ol>
-        </nav>
+              </li>
+              <li className="flex items-center justify-between px-3 py-2">
+                <div className="flex items-center">
+                  <button type="button" className="svg-container mr-2 h-5 w-4">
+                    <img src={iconArrowRight} alt="icon arrow right" />
+                  </button>
+                  <span className="cursor-pointer font-semibold text-blue hover:text-[#5B7FCD]">
+                    NOTEBOOKS
+                  </span>
+                </div>
+                <button type="button" className="mr-2 h-5 w-5">
+                  <img src={iconAddBlue} alt="icon add blue" />
+                </button>
+              </li>
+            </ol>
+          </nav>
+        ) : undefined}
         <main>
           <Outlet />
         </main>
