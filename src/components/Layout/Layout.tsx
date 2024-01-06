01241238ac4446402f33e6b53dfeda20d6c1dcad
eun0leee <eun0leee@gmail.com>
2024-01-06 09:10:00 +0900

    feat : 레이아웃 마크업 (#4)

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,23 @@
 import { Outlet } from 'react-router-dom';
 
+import iconHamburger from '@/assets/icon_hamburger.svg';
+
 const Layout = () => {
   return (
     <>
-      <header>헤더</header>
+      <header className="flex justify-between">
+        <button type="button" className="h-5 w-4">
+          <img src={iconHamburger} alt="icon hamburger" />
+        </button>
+        <button type="button">New Note</button>
+      </header>
       <div className="flex">
-        <nav>내비게이션</nav>
+        <nav className="w-52">
+          <ul>
+            <li>All Notes</li>
+            <li>NOTEBOOKS</li>
+          </ul>
+        </nav>
         <main>
           <Outlet />
         </main>
