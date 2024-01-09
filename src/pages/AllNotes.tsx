d73eef83c27b8023595726695615da778f02db43
eun0leee <eun0leee@gmail.com>
2024-01-09 16:51:42 +0900

    bug : build type error (#10)

--- src/pages/AllNotes.tsx
+++ src/pages/AllNotes.tsx
@@ -5,7 +5,7 @@ import NoResults from '@components/common/NoResults';
 
 const AllNotes = () => {
   const memos = getMemo();
-  const firstMemoCreatedAt = memos[memos.length - 1].createdAt;
+  const firstMemoCreatedAt = memos[memos.length - 1]?.createdAt;
 
   return (
     // eslint-disable-next-line react/jsx-no-useless-fragment
