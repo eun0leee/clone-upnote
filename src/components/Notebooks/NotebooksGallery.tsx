186b2ad7a680a834ca42d2eb9e099967849007ee
eun0leee <eun0leee@gmail.com>
2024-01-15 00:04:34 +0900

    feat : note gallery item 줄바꿈 스타일링 (#14)

--- src/components/Notebooks/NotebooksGallery.tsx
+++ src/components/Notebooks/NotebooksGallery.tsx
@@ -12,29 +12,30 @@ const NotebooksGallery = () => {
   const notebooks = getNotebooks();
 
   return (
-    <ol className="flex w-full gap-10 overflow-hidden px-7 py-4">
+    <ol className="flex flex-wrap gap-10 px-7 py-4">
       {notebooks.reverse().map((notebook: NotebooksProps) => (
-        <Link
-          to={`/notebooks/${notebook.title}`}
-          key={notebook.title}
-          className="relative"
-          onMouseEnter={() => setHoveredNotebook(notebook.title)}
-          onMouseLeave={() => setHoveredNotebook(null)}
-        >
-          <img src={imgNotebook} alt="notebook" className="min-w-28" />
-          <span className="absolute bottom-0 h-10 w-full truncate rounded-b-[10px] bg-black bg-opacity-30 px-2 py-2 font-semibold text-gray-300">
-            {notebook.title}
-          </span>
-          {hoveredNotebook === notebook.title ? (
-            <button
-              type="button"
-              onClick={(e) => handleDeleteNotebook(e, notebook.title)}
-              className="absolute right-2 top-1 z-10 flex h-8 w-8 items-center justify-center rounded-full bg-black bg-opacity-30 hover:bg-opacity-50"
-            >
-              <img src={iconDelete} alt="icon delete" className="w-3" />
-            </button>
-          ) : undefined}
-        </Link>
+        <li key={notebook.title}>
+          <Link
+            to={`/notebooks/${notebook.title}`}
+            className="relative"
+            onMouseEnter={() => setHoveredNotebook(notebook.title)}
+            onMouseLeave={() => setHoveredNotebook(null)}
+          >
+            <img src={imgNotebook} alt="notebook" className="min-w-28" />
+            <span className="absolute bottom-0 h-10 w-full flex-wrap truncate rounded-b-[10px] bg-black bg-opacity-30 px-2 py-2 font-semibold text-gray-300">
+              {notebook.title}
+            </span>
+            {hoveredNotebook === notebook.title ? (
+              <button
+                type="button"
+                onClick={(e) => handleDeleteNotebook(e, notebook.title)}
+                className="absolute right-2 top-1 z-10 flex h-8 w-8 items-center justify-center rounded-full bg-black bg-opacity-30 hover:bg-opacity-50"
+              >
+                <img src={iconDelete} alt="icon delete" className="w-3" />
+              </button>
+            ) : undefined}
+          </Link>
+        </li>
       ))}
     </ol>
   );
